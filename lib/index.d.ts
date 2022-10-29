46e96b23ffa0eb2a88cb8e6158d54e4ca99b6d37
LuisChiej <54555805+LuisChiej@users.noreply.github.com>
2022-10-29 14:39:27 +0100

    added `account` service

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,7 @@ import AirtimeService from "./services/airtime.service";
 import CableService from "./services/cable.service";
 import DataBundleService from "./services/databundle.service";
 import ElectricityService from "./services/electricity.service";
+import AccountService from "./services/account.service";
 export default class Baxi {
     private _apiKey;
     private _environment;
@@ -11,6 +12,7 @@ export default class Baxi {
     cable: CableService;
     airtime: AirtimeService;
     dataBundle: DataBundleService;
+    account: AccountService;
     constructor(apiKey: string, environment: 'development' | 'production');
     axios(): AxiosInstance;
 }
---
+++ lib/services/account.service.d.ts
@@ -0,0 +1,7 @@
+import Baxi from "..";
+export default class AccountService {
+    #private;
+    constructor(baxi: Baxi);
+    getBalance(): Promise<any>;
+}
+//# sourceMappingURL=account.service.d.ts.map
