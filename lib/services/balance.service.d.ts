e2fd660b72d0f1eb8c4cce34af74497f7d4dc5ec
LuisChiej <54555805+LuisChiej@users.noreply.github.com>
2022-10-29 14:25:17 +0100

    added `balance` service

---
+++ lib/services/balance.service.d.ts
@@ -0,0 +1,7 @@
+import Baxi from "..";
+export default class BalanceService {
+    #private;
+    constructor(baxi: Baxi);
+    getBalance(): Promise<any>;
+}
+//# sourceMappingURL=balance.service.d.ts.map
---
+++ src/services/balance.service.ts
@@ -0,0 +1,20 @@
+import Baxi from "..";
+
+export default class BalanceService {
+    #baxi: Baxi;
+    
+    constructor(baxi: Baxi) {
+        this.#baxi = baxi;
+    }
+
+    async getBalance(): Promise<any> {
+        const url: string = 'services/superagent/account/balance';
+
+        try {
+            const response = await this.#baxi.axios().get(url);
+            if(response.status === 200) return response.data;
+        } catch (e) {
+            return null;
+        }
+    }
+}
